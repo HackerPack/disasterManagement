ec78f9705603a545fada60d61c84f188e0f8ab26
Diksha <Diksha@Dikshas-MacBook-Air.local>
2016-01-30 23:33:28 -0500

    Making few changes

--- js/database.js
+++ js/database.js
@@ -110,7 +110,7 @@ function getCompletedTasks(uid, callback){
   var bookRef = new Firebase(FIRE_BASE_URL+TASKS_TABLE);
   bookRef.orderByChild("Finished").equalTo(uid).on("value", function(snapshot) {
     snapshot.forEach(function(data){
-      return_data.push(data.val());
+      return_data.push(data);
     });
     callback(return_data);
   });
@@ -133,38 +133,6 @@ getUser('facebook:10153182851271621', function(data){
         //console.log(innerData.fname);
     });
 });
-function getTwitterWidgetAll(callback){
-  //alert("Fetching widget details");
-  var twitterRef = new Firebase(FIRE_BASE_URL+HASHTAGS_TABLE+hashtag);
-  var searchResult = [];
-
-  //twitterRef.orderByKey().equalTo(hashtag).on("value", function(snapshot) {
-    twitterRef.on('value', function(data){
-      data.forEach(function(childSnapshot) {
-      //console.log(data.val());
-      searchResult.push(childSnapshot);
-     //callback(searchResult);
-     /*snapshot.forEach(function(childSnapshot) {
-        var temp = JSON.stringify(childSnapshot.val());
-        if(hashtag){
-          var n = temp.search(hashtag);
-          if(n>-1){
-            searchResult.push(childSnapshot.val());
-          }
-        }
-        else{
-         searchResult.push(childSnapshot.val());
-        }
-      });*/
-    $.each(data.val(), function(key, value){
-      //console.log(value);
-    })
-     //console.log(searchResult);
-          //alert(searchResult);
-     callback(searchResult);
-     return searchResult
-  });  
-}
 
 function getTwitterWidget(hashtag, callback){
   //alert("Fetching widget details");
